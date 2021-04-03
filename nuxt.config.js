85a98b44883ec6e3fd98aa0f8a7a372bb7231274
Koray Güler <us.koray.guler@gmail.com>
2021-04-03 14:46:01 +0300

    update

--- nuxt.config.js
+++ nuxt.config.js
@@ -15,7 +15,7 @@ export default {
 
   // Global CSS: https://go.nuxtjs.dev/config-css
   css: [],
-
+  ssr: false,
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [],
 
@@ -27,7 +27,10 @@ export default {
     // https://go.nuxtjs.dev/tailwindcss
     "@nuxtjs/tailwindcss"
   ],
-
+  generate: {
+    dir: "dist",
+    subFolders: false
+  },
   // Modules: https://go.nuxtjs.dev/config-modules
   modules: [
     [
@@ -45,8 +48,7 @@ export default {
         ]
       }
     ]
-  ],
+  ]
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
-  build: {}
 };
